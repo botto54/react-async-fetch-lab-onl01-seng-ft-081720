00308fd179c677f8110cbbb5e809954f135f32e2
botto54 <bryanlbottorff@gmail.com>
2021-01-18 23:39:00 -0500

    commit tree ea2de1d871dc11f5af37c959c771bfc4f70a736f

--- src/App.js
+++ src/App.js
@@ -2,9 +2,23 @@
 import React from 'react'
 
 class App extends React.Component {
+    state = {
+        peopleSpace: []
+    }
 
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json')
+        .then(resp => resp.json())
+        .then(({people}) => this.setState({peopleSpace: people}))
+    }
 
     render(){
-        
+        return(
+            <div>
+                {this.state.peopleSpace.map((person, id) => <h1 key={id}>{person.name}</h1>)}
+            </div>
+        )
     }
 }
+
+export default App
